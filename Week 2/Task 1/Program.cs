0d63fcfe606573fac50a3e48abacef2ba7171e13
Anna <renita5701@gmail.com>
2019-02-05 10:55:14 +0600

    ninth commit

--- Week 2/Task 1/Program.cs
+++ Week 2/Task 1/Program.cs
@@ -16,23 +16,22 @@ namespace Task_1
 
         private static void F2()
         {
-            FileStream fs = new FileStream(@"C:\Users\User\Documents\KBTU\PP2\palin.txt", FileMode.Open, FileAccess.Read);
-            StreamReader sr = new StreamReader(fs);
+            FileStream fs = new FileStream(@"C:\Users\User\Documents\KBTU\PP2\palin.txt", FileMode.Open, FileAccess.Read); // Provides a Stream for a file, supporting both synchronous and asynchronous read and write operations
+            StreamReader sr = new StreamReader(fs); // Implements a TextReader that reads characters from a byte stream; reading from a file
 
             int counter = 0;
-            string text = sr.ReadToEnd();
-            string[] lines = text.Split();
+            string text = sr.ReadToEnd(); //reading the string
 
-            for (int i=0; i< lines.Length; i++)
+            for (int i=0; i< text.Length; i++)
             {
-                if (lines[i] == lines[lines.Length - i - 1])
+                if (text[i] == text[text.Length - i - 1])
                 {
                     counter++;
                 }
                 else counter = 0;
             }
 
-            if (counter == lines.Length)
+            if (counter == text.Length)
             {
                 Console.WriteLine("Yes");
             }
--- Week 2/Task 2/Program.cs
+++ Week 2/Task 2/Program.cs
@@ -10,7 +10,7 @@ namespace Task_2
     class Program
     {
 
-        static bool IsPrime(int x)
+        static bool IsPrime(int x) // function determine if prime or not
         {
             if (x == 1) return false;
             if (x == 2) return true;
@@ -28,40 +28,39 @@ namespace Task_2
             return functionResult;
         }
 
-        static bool IsPrimeString(string s)
+        static bool IsPrimeString(string s) // pass converted element of string into int to IsPrime
         {
             return IsPrime(int.Parse(s));
         }
 
         static void Main(string[] args)
         {
-
             List<string> res = new List<string>();
 
             FileStream fs = new FileStream(@"C:\Users\User\Documents\KBTU\PP2\numb.txt", FileMode.Open, FileAccess.Read);
             StreamReader sr = new StreamReader(fs);
 
-            string line = sr.ReadLine();
+            string line = sr.ReadLine(); // reading the line
             string[] nums = line.Split(' ');
 
-            foreach (var x in nums)
+            foreach (var x in nums) // for each element in array nums
             {
-                if (IsPrimeString(x))
+                if (IsPrimeString(x)) // function if prime
                 {
-                    res.Add(x);
+                    res.Add(x); // adding result to list res
                 }
             }
 
             sr.Close();
             fs.Close();
 
-            FileStream fs2 = new FileStream(@"C:\Users\User\Documents\KBTU\PP2\prime.txt", FileMode.Create, FileAccess.Write);
-            StreamWriter sw = new StreamWriter(fs2);
+            FileStream fs2 = new FileStream(@"C:\Users\User\Documents\KBTU\PP2\prime.txt", FileMode.Create, FileAccess.Write); //file stream for writing
+            StreamWriter sw = new StreamWriter(fs2); // class streamwriter for writing to text file
 
-            foreach (var x in res)
+            foreach (var x in res) 
             {
-                sw.Write(x + " ");
-                Console.Write(x + " ");
+                sw.Write(x + " "); // writing result to text file
+                Console.Write(x + " "); // writing result to console
             }
 
             sw.Close();
