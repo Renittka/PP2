aa7a70a4503c1054b145d0f13ffe0603113c507a
Anna <renita5701@gmail.com>
2019-02-05 08:28:48 +0600

    eight commit

--- Week 2/Task 1/Program.cs
+++ Week 2/Task 1/Program.cs
@@ -8,45 +8,40 @@ using System.IO;
 namespace Task_1
 {
     class Program
-    { 
+    {
+        static void Main(string[] args)
+        {
+            F2();
+        }
 
-            static void Main(string[] args)
+        private static void F2()
+        {
+            FileStream fs = new FileStream(@"C:\Users\User\Documents\KBTU\PP2\palin.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            int counter = 0;
+            string text = sr.ReadToEnd();
+            string[] lines = text.Split();
+
+            for (int i=0; i< lines.Length; i++)
             {
-                F2();
-            }
-
-            private static void F2()
-            {
-                FileStream fs = new FileStream(@"C:\Users\User\Documents\KBTU\PP2\palin.txt", FileMode.Open, FileAccess.Read);
-                StreamReader sr = new StreamReader(fs);
-
-                string text = sr.ReadToEnd();
-                string[] lines = text.Split('\n');
-
-                Console.WriteLine(lines[2]);
-
-                sr.Close();
-                fs.Close();
-            }
-
-            private static void F1()
-            {
-                FileStream fs = new FileStream(@"C:\Users\User\Documents\KBTU\PP2\palin.txt", FileMode.Open, FileAccess.Read);
-                StreamReader sr = new StreamReader(fs);
-
-                string line = "";
-                int cnt = 0;
-                while (!sr.EndOfStream && cnt <= 2)
+                if (lines[i] == lines[lines.Length - i - 1])
                 {
-                    line = sr.ReadLine();
-                    cnt++;
+                    counter++;
                 }
-
-                Console.WriteLine(line);
-
-                sr.Close();
-                fs.Close();
+                else counter = 0;
             }
+
+            if (counter == lines.Length)
+            {
+                Console.WriteLine("Yes");
+            }
+            else Console.WriteLine("No");
+
+            sr.Close();
+            fs.Close();
+        }
+
      }
 }
  
