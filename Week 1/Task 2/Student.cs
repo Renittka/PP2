74ec0120b766b809e4b14a193f4ea78a46b873b3
Anna <renita5701@gmail.com>
2019-02-14 18:16:02 +0600

    upd

---
+++ Week 1/Task 2/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Student // class for define name, id and increment the year
+    {
+        protected string name;
+        protected string id;
+        protected int year;
+
+        public Student(string name, string id) // class with 2 parameters: name, id
+        {
+            this.name = name;
+            this.id = id;
+            year = 2018;
+        }
+
+        public void PrintInfo() // access to name, id and year
+        {
+            Console.WriteLine(name + " " + id + " " + year);
+        }
+
+        public void IncYear() // increases year by 1
+        {
+            year++;
+        }
+    }
+}
---
+++ Week 1/Task 3/ArrayOut.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class ArrayOut  // class is used to double the array elements 
+    {
+        protected int[] Arr;
+        protected int n;
+
+        /*ArrayOut constructor with two parameters: array a and array length n
+          with public access modifier (accessible from anywhere in the code)*/
+        public ArrayOut(int[] Arr, int n)
+        {
+            this.Arr = Arr;
+            this.n = n;
+        }
+
+        public void PrintInfo() // method is used to print numbers twice
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Arr[i] + " ");
+                Console.Write(Arr[i] + " ");
+            }
+        }
+    }
+}
