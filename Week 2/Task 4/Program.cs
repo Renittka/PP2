08002e5b48d12bf16195557c3704795eadbfa757
Anna <renita5701@gmail.com>
2019-02-14 22:11:26 +0600

    upd

--- Week 2/Task 4/Program.cs
+++ Week 2/Task 4/Program.cs
@@ -14,48 +14,15 @@ namespace Task_4
             string sourceDir = @"C:\Users\User\Documents\KBTU\PP2";
             string backupDir = @"C:\Users\User\Documents";
             string resultfile = @"C:\Users\User\Documents\MyTest.txt";
+            string name = "MyTest.txt";
 
-            if (File.Exists(resultfile))
+            if (File.Exists(resultfile)) // if file exist in destination - delete
             {
                 File.Delete(resultfile);
             }
-
-            try
-            {
-                string[] txtList = Directory.GetFiles(sourceDir, "MyTest.txt");
                 // Copy text files.
-                foreach (string f in txtList)
-                {
-                    // Remove path from the file name.
-                    string fName = f.Substring(sourceDir.Length + 1);
-
-                    try
-                    {
-                        // Will not overwrite if the destination file already exists.
-                        File.Copy(Path.Combine(sourceDir, fName), Path.Combine(backupDir, fName));
-                        //File.Copy(sourceDir, backupDir);
-                    }
-
-
-                    // Catch exception if the file was already copied.
-                    catch (IOException copyError)
-                    {
-                        Console.WriteLine(copyError.Message);
-                    }
-
-                }
-
-                // Delete source files that were copied.
-                foreach (string f in txtList)
-                {
-                    File.Delete(f);
-                }
-            }
-
-            catch (DirectoryNotFoundException dirNotFound)
-            {
-                Console.WriteLine(dirNotFound.Message);
-            }
+                File.Copy(Path.Combine(sourceDir, name), Path.Combine(backupDir, name)); // Combine name of file and path to copy from and to
+                File.Delete(sourceDir);
         }
 
         public static void Main()
@@ -70,8 +37,7 @@ namespace Task_4
                     File.Delete(path);
                 }
 
-                // Create the file.
-                using (FileStream fs = File.Create(path))
+                using (FileStream fs = File.Create(path)) // Create the file.
                 {
 
                 }
