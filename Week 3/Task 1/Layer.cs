2ca410e5f08b5bd37c56b66433c926069504289d
Anna <renita5701@gmail.com>
2019-02-16 08:02:40 +0600

    upd

--- Week 3/Task 1/Layer.cs
+++ Week 3/Task 1/Layer.cs
@@ -9,38 +9,40 @@ namespace Task_1
 {
     class Layer
     {
-      
-        public FileSystemInfo[] Content
+        public FileSystemInfo[] Content // attributes for the current file or directory
         {
             get;
             set;
         }
-        public int SelectedItem
+        public int SelectedItem // // attributes for the index
         {
             get;
             set;
         }
 
-        public void Draw()
+        public void Draw() // method for drawing the interface
         {
-            Console.BackgroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Black; // interface of console
             Console.Clear();
-            for (int i = 0; i < Content.Length; ++i)
+
+            for (int i = 0; i < Content.Length; i++)
             {
                 if (i == SelectedItem)
                 {
-                    Console.BackgroundColor = ConsoleColor.Magenta;
+                    Console.BackgroundColor = ConsoleColor.Magenta; // background of selected item
                 }
-                else Console.BackgroundColor = ConsoleColor.Black;
+                else Console.BackgroundColor = ConsoleColor.Black; // background of nonselected item
+
                 if (Content[i].GetType() == typeof(DirectoryInfo))
                 {
-                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.White; // if dir - white color
                 }
                 else
                 {
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.ForegroundColor = ConsoleColor.DarkYellow; // file - yellow color
                 }
-                Console.WriteLine(i + 1 + ". " + Content[i].Name);
+
+                Console.WriteLine(i + 1 + ". " + Content[i].Name); // index + name of dir or file
             }
         }
     }
--- Week 3/Task 1/Program.cs
+++ Week 3/Task 1/Program.cs
@@ -18,59 +18,61 @@ namespace Task_1
         static void Main(string[] args)
         {
             DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\User\Documents\Test");
-            Stack<Layer> history = new Stack<Layer>();
+            Stack<Layer> history = new Stack<Layer>(); // layer for displaying folder's content
 
             history.Push(
                 new Layer
                 {
                     Content = dirInfo.GetFileSystemInfos() // get files and folders
-            }
-            );
+                }
+                );
+
             Viewmode viewMode = Viewmode.ShowDirContent; // default case - folder
 
-            bool quit = false;
+            bool quit = false; 
             while (!quit)
             {
                 if (viewMode == Viewmode.ShowDirContent)
                 {
                     history.Peek().Draw(); // drawing the interface
                 }
+
                 ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
                 switch (consoleKeyInfo.Key) // description of main keys 
                 {
                     case ConsoleKey.UpArrow:
-                        if (history.Peek().SelectedItem -1 < 0)
+                        if (history.Peek().SelectedItem -1 < 0) // top -> bottom
                         {
                             history.Peek().SelectedItem = history.Peek().Content.Length - 1; 
                         }
                         else
                         {
-                            history.Peek().SelectedItem--;
+                            history.Peek().SelectedItem--; // index - 1
                         }
                         break;
 
                     case ConsoleKey.DownArrow:
-                        if (history.Peek().SelectedItem +1 >= history.Peek().Content.Length)
+                        if (history.Peek().SelectedItem +1 >= history.Peek().Content.Length) // bottom -> top
                         {
                             history.Peek().SelectedItem = 0;
                         }
                             else
                         {
-                            history.Peek().SelectedItem++;
+                            history.Peek().SelectedItem++; // index + 1
                         }
                         break;
 
                     case ConsoleKey.Enter:
-                        int x = history.Peek().SelectedItem;
-                        FileSystemInfo fileSystemInfo = history.Peek().Content[x];
+                        int x = history.Peek().SelectedItem; // index of selected item
+                        FileSystemInfo fileSystemInfo = history.Peek().Content[x]; // content of selected item
 
-                        if(fileSystemInfo.GetType() == typeof(DirectoryInfo)) // if directory - getting its content
+                        if (fileSystemInfo.GetType() == typeof(DirectoryInfo)) // if directory - getting its content
                         {
                             viewMode = Viewmode.ShowDirContent;
                             DirectoryInfo selectedDir = fileSystemInfo as DirectoryInfo; // selected item is directory
                             history.Push(new Layer
                             {
-                                Content = selectedDir.GetFileSystemInfos() 
+                                Content = selectedDir.GetFileSystemInfos() // getting its content
 
                             });
                         }
@@ -85,7 +87,7 @@ namespace Task_1
                                     Console.BackgroundColor = ConsoleColor.White;
                                     Console.Clear();
                                     Console.ForegroundColor = ConsoleColor.Black;
-                                    Console.WriteLine(sr.ReadToEnd());
+                                    Console.WriteLine(sr.ReadToEnd()); // read file to end
                                 }
                             }
                         }
@@ -93,39 +95,41 @@ namespace Task_1
 
                     case ConsoleKey.F6: //rename
                         Console.BackgroundColor = ConsoleColor.Black;
-                        Console.Clear();
+                        Console.Clear(); // clear the display
                         string name = Console.ReadLine();
-                        int x3 = history.Peek().SelectedItem;
-                        FileSystemInfo fileSystemInfo3 =  history.Peek().Content[x3];
+                        int x3 = history.Peek().SelectedItem; // index of selected item
+                        FileSystemInfo fileSystemInfo3 =  history.Peek().Content[x3]; // content of selected item
+
                         if (fileSystemInfo3.GetType() == typeof(DirectoryInfo))
-                        {
-                            DirectoryInfo directoryInfo = fileSystemInfo3 as DirectoryInfo;
+                        {   // if directory - move to the directory with new name
+                            DirectoryInfo directoryInfo = fileSystemInfo3 as DirectoryInfo; 
                             Directory.Move(fileSystemInfo3.FullName, directoryInfo.Parent.FullName + "/" + name);
-                            history.Peek().Content = directoryInfo.Parent.GetFileSystemInfos();
+                            history.Peek().Content = directoryInfo.Parent.GetFileSystemInfos(); // getting content with new name of dir
                         }
                         else
-                        {
+                        {   // if file - move to the directory with new name
                             FileInfo fileInfo = fileSystemInfo3 as FileInfo;
                             File.Move(fileSystemInfo3.FullName, fileInfo.Directory.FullName + "/" + name);
-                            history.Peek().Content = fileInfo.Directory.GetFileSystemInfos();
+                            history.Peek().Content = fileInfo.Directory.GetFileSystemInfos(); // getting content with new name of file
                         }
                         break;
 
                     case ConsoleKey.F8: // delete
-                        int x2 = history.Peek().SelectedItem;
-                        FileSystemInfo fileSystemInfo2 = history.Peek().Content[x2];
-                            history.Peek().SelectedItem--;
-                        if (fileSystemInfo2.GetType() == typeof(DirectoryInfo))
+                        int x2 = history.Peek().SelectedItem; // index of selected item
+                        FileSystemInfo fileSystemInfo2 = history.Peek().Content[x2]; // content of selected item
+                        history.Peek().SelectedItem--;
+
+                        if (fileSystemInfo2.GetType() == typeof(DirectoryInfo)) // if dir - delete using path
                         {
                             DirectoryInfo directoryInfo = fileSystemInfo2 as DirectoryInfo;
                             Directory.Delete(fileSystemInfo2.FullName, true); // if folder is not empty (true) delete
-                            history.Peek().Content = directoryInfo.Parent.GetFileSystemInfos();
+                            history.Peek().Content = directoryInfo.Parent.GetFileSystemInfos(); // content without dir
                         }
                         else
-                        {
+                        {   // else delete file using path
                             FileInfo fileInfo = fileSystemInfo2 as FileInfo;
-                            File.Delete(fileSystemInfo2.FullName);
-                            history.Peek().Content = fileInfo.Directory.GetFileSystemInfos();
+                            File.Delete(fileSystemInfo2.FullName); 
+                            history.Peek().Content = fileInfo.Directory.GetFileSystemInfos(); // get content of dir without file
                         }
                         break;
 
@@ -135,7 +139,7 @@ namespace Task_1
                             history.Pop(); // clear the last layer
                         }
                         else
-                        { // if file - draw interface and set diectory mode
+                        { // if file - draw interface and set directory mode
                             Console.BackgroundColor = ConsoleColor.Black;
                             Console.Clear();
                             Console.ForegroundColor = ConsoleColor.White;
@@ -143,7 +147,7 @@ namespace Task_1
                         }
                         break;
 
-                    case ConsoleKey.Escape:
+                    case ConsoleKey.Escape: // exit the program
                         quit = true;
                         break;
                 }
