e554d31e1cdc330e7d930685504b2c934d06eddc
Anna <renita5701@gmail.com>
2019-03-28 21:15:00 +0600

    upd

---
+++ Week 7/Task 6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            System.Timers.Timer timer = new System.Timers.Timer(1000);
+            timer.Elapsed += DoIt;
+            timer.Enabled = true;
+            timer.Start();
+            Thread.CurrentThread.Join();
+        }
+        private static void DoIt(object sender, ElapsedEventArgs e)
+        {
+            int x = 0;
+            int y = 0;
+            while (true)
+            {
+
+            }
+        }
+    }
+}
+
