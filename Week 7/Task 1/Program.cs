1a6f4c35ae2e23db2d5c507b46c65e4ad5c09c31
Anna <renita5701@gmail.com>
2019-03-28 12:08:31 +0600

    upd

---
+++ Week 7/Task 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace thread3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            Thread[] t = {
+                new Thread(array),
+                new Thread(array),
+                new Thread(array) };
+
+            for (int i = 0; i < n; i++)
+            {
+                t[i].Name = "T" + i;
+                t[i].Start();
+
+            }
+            Console.ReadKey();
+
+        }
+        public static void array()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+                Thread.Sleep(3000);
+            }
+
+        }
+    }
+}
---
+++ Week 7/Task 2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace thread4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyThread t1 = new MyThread("Thread 1");
+            MyThread t2 = new MyThread("Thread 2");
+            MyThread t3 = new MyThread("Thread 3");
+
+            t1.startThread();
+            t2.startThread();
+            t3.startThread();
+            Console.Read();
+
+        }
+    }
+    class MyThread
+    {
+        Thread threadfield;
+
+        public MyThread(string name)
+        {
+            threadfield = new Thread(this.func);
+            threadfield.Name = name;
+        }
+
+        public void startThread()
+        {
+            threadfield.Start();
+
+        }
+        void func()
+        {
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " выводит " + i);
+                Thread.Sleep(300);
+            }
+            Console.WriteLine(Thread.CurrentThread.Name + " завершился");
+        }
+    }
+
+}
+
