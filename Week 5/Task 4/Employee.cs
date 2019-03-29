d0719bf74b4b7320a7df0d5e6698e97949787658
Anna <renita5701@gmail.com>
2019-03-29 15:17:32 +0600

    upd

---
+++ Week 5/Task 4/Employee.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    public class Employee
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        private int id;
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                this.id = value;
+            }
+        }
+        private int salary;
+        public int Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            set
+            {
+                this.salary = value;
+            }
+        }
+        public void Increase(int salary)
+        {
+            this.salary = this.salary + 50000;
+        }
+        public override string ToString()
+        {
+            return name + " " + id + " " + salary;
+        }
+        public Employee()
+        {
+
+        }
+
+    }
+}
---
+++ Week 5/Task 4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            e1.Name = "AAAA";
+            e1.Id = 1;
+            e1.Salary = 50000;
+            e1.Increase(e1.Salary);
+            e1.Increase(e1.Salary);
+            Ser(e1);
+            Des();
+        }
+        static void Ser(Employee e1)
+        {
+            FileStream fs = new FileStream("employee.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Employee));
+            xs.Serialize(fs, e1);
+            fs.Close();
+        }
+        static void Des()
+        {
+            FileStream fs = new FileStream("employee.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Employee));
+            Employee e1des = xs.Deserialize(fs) as Employee;
+            fs.Close();
+            Console.WriteLine(e1des);
+        }
+    }
+
+}
